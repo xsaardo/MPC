a22e99462ebe9a085b0a089e5d718ee0ff808aaa
Corey MacBook Pro <coreyearleon@icloud.com>
2016-04-11 15:29:25 -0700

    Initial Commit

---
+++ mpc_chatTests/mpc_chatTests.swift
@@ -0,0 +1,36 @@
+//
+//  mpc_chatTests.swift
+//  mpc_chatTests
+//
+//  Created by Corey Baker on 4/11/16.
+//  Copyright © 2016 University of California San Diego - ECE 158B. All rights reserved.
+//
+
+import XCTest
+@testable import mpc_chat
+
+class mpc_chatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
